f1779527772e6bb0a1103ba8464950588982806c
mail@jdsherrington.com <mail@jdsherrington.com>
2025-02-13 09:37:44 +1100

    Fixed tag styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,45 +9,7 @@ export default {
 			lg: "905px", // 12 columns
 			md: "600px", // 8 columns
 			sm: "0px", // 4 columns
-		},
-		fontSize: {
-			xs: "0.75rem",
-			sm: "0.875rem",
-			base: "1rem",
-			lg: "1.125rem",
-			xl: "1.25rem",
-			"2xl": "1.5rem",
-			"3xl": "1.875rem",
-			"4xl": "2.25rem",
-			"5xl": "3rem",
-            "6xl": "3.75rem",
-		},
-		lineHeight: {
-			"3xs": "0.7rem",
-			"2xs": "0.75rem",
-			xs: "1rem",
-			sm: "1.25rem",
-			base: "1.5rem",
-			lg: "1.75rem",
-			xl: "1.85rem",
-			"2xl": "2rem",
-			"3xl": "2.25rem",
-			"4xl": "2.5rem",
-			1: "1",
-			none: "0",
-			normal: "1.5",
-		},
-		letterSpacing: {
-			extratight: "-0.15rem",
-			tightest: "-0.1rem",
-			tighter: "-0.05rem",
-			tight: "-0.025rem",
-			base: "0rem",
-			wide: "0.05rem",
-			wider: "0.1rem",
-			widest: "0.15rem",
-			extrawide: "0.2rem",
-		},
+		},	
 		container: {
 			center: true,
 			padding: "2rem",
@@ -115,7 +77,7 @@ export default {
 			},
 			fontFamily: {
 				sans: ["Fluent"],
-				mono: ["DisketMono"],
+				mono: ["JetBrainsMono"],
 				code: ["JetBrainsMono"],
 			},
 			keyframes: {
